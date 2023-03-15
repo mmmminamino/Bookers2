70d1115606b1e927dc8830e1f786ee12fc3d5e71
mmmminamino <124233935+mmmminamino@users.noreply.github.com>
2023-03-15 15:01:22 +0000

    [途中]bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:"homes#top"
-  
+
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update, :index]
   
   get 'homes/about' => 'homes#about', as:'about'
+  
 end
